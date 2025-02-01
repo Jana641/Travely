407c556a5e176ea212bf666d31e5b2e424dd98e5
Jana641 <101220641@mu.edu.lb>
2025-02-01 15:18:55 +0200

    implememnted the template including homepage

--- app/Http/Controllers/FlightController.php
+++ app/Http/Controllers/FlightController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\Flight; 
 
 use Illuminate\Http\Request;
 
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,479 @@
 @extends('layouts.app')
 
 @section('content')
-<!-- <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+<!DOCTYPE html>
+<html lang="en">
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+<!-- Mirrored from jufailitech.com/envatoitems/travilo/html/index-2.html by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 14 Nov 2023 12:52:30 GMT -->
+<head>
+	<meta charset="utf-8">
+	<!-- Responsive -->
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
-        </div>
-    </div>
-</div> -->
+	<title>Travilo - Home 02</title>
+
+	<link href="resources/images/favicon.png" rel="shortcut icon" type="image/x-icon">
+
+	<!-- Google Fonts -->
+	<link rel="preconnect" href="https://fonts.googleapis.com/">
+	<link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Open+Sans&amp;display=swap" rel="stylesheet">
+
+	<!-- Stylesheets -->
+	<link href="./css/lib/bootstrap/bootstrap.min.css" rel="stylesheet">
+	<link href="./css/lib/fontawesome/css/fontawesome.min.css" rel="stylesheet">
+	<link href="./css/lib/fontawesome/css/brands.css" rel="stylesheet">
+	<link href="./css/lib/fontawesome/css/solid.css" rel="stylesheet">
+	<link href="./css/lib/simple-line-icons/css/simple-line-icons.min.css" rel="stylesheet">
+	<link href="./css/lib/flaticon/css/flaticon.min.css" rel="stylesheet">
+	<link href="./css/lib/animate.min.css" rel="stylesheet">
+	<link href="./css/lib/slick.min.css" rel="stylesheet">
+	<link href="./css/lib/swiper.min.css" rel="stylesheet">
+	<link href="./css/lib/jquery-ui.min.css" rel="stylesheet">
+	<link href="./css/lib/jquery.fancybox.min.css" rel="stylesheet">
+	<link href="./css/custom-animate.css" rel="stylesheet">
+	<link href="./css/style.css" rel="stylesheet">
+	<link href="./css/responsive.css" rel="stylesheet">
+    
+</head>
+
+<body>
+	<div class="page-wrapper">
+		<!-- Preloader -->
+		<div class="loader-wrap">
+			<div class="preloader">
+				<div class="preloader-close">x</div>
+				<div id="handle-preloader" class="handle-preloader">
+					<div class="animation-preloader">
+						<div class="txt-loading">
+							<span data-text-preloader="T" class="letters-loading">T</span>
+							<span data-text-preloader="R" class="letters-loading">R</span>
+							<span data-text-preloader="A" class="letters-loading">A</span>
+							<span data-text-preloader="V" class="letters-loading">V</span>
+							<span data-text-preloader="E" class="letters-loading">E</span>
+							<span data-text-preloader="L" class="letters-loading">L</span>
+							<span data-text-preloader="Y" class="letters-loading">Y</span>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<!--Menu Backdrop-->
+		<div class="menu-backdrop"></div>
+
+		<!-- Hidden Navigation Bar -->
+		<div class="hidden-bar">
+			<!-- Hidden Bar Wrapper -->
+			<div class="hidden-bar-wrapper">
+				<div class="hidden-bar-closer"><span class="icon"><svg class="icon-close" role="presentation"
+							viewBox="0 0 16 14">
+							<path d="M15 0L1 14m14 0L1 0" stroke="currentColor" fill="none" fill-rule="evenodd"></path>
+						</svg></span></div>
+				<div class="nav-logo-box">
+					<!-- logo will be copied here ! -->
+				</div>
+				<!-- .Side-menu -->
+				<nav class="side-menu">
+					<!-- main-menu will be copied here! -->
+				</nav><!-- .side-menu -->
+
+				<div class="links-box clearfix">
+					<div class="clearfix">
+						<div class="link"><a href="contact.html" class="theme-btn btn-style-one"><span>Login /
+									Signup</span></a></div>
+					</div>
+				</div>
+
+			</div><!-- / Hidden Bar Wrapper -->
+		</div>
+		<!-- / Hidden Bar -->
+
+
+		<!-- Banner Section -->
+		<div class="banner-two">
+			<div class="banner-container">
+				<div class="banner-slider">
+					<div class="swiper-wrapper">
+						<!--Slide Item-->
+						<div class="swiper-slide slide-item">
+							<div class="image-layer"
+								style="background-image: url(./images/resources/slider/main-slider-1.jpg);"></div>
+							<div class="auto-container">
+								<div class="content-box">
+									<div class="content">
+										<div class="clearfix">
+											<div class="inner">
+												<h1><span>Adventure<i class="s-text">Adventure</i></span></h1>
+												<div class="travilo-text">Disover top places and enjoy extreme
+													activities around the world with a cheap price.</div>
+												<div class="links-box clearfix">
+													<div class="link"><a href="about.html"
+															class="theme-btn btn-style-two"><span>Explore
+																Now</span></a>
+													</div>
+													<div class="link"><a href="about.html"
+															class="theme-btn btn-style-three"><span>About
+																Us</span></a>
+													</div>
+												</div>
+											</div>
+
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<!--Slide Item-->
+						<div class="swiper-slide slide-item">
+							<div class="image-layer"
+								style="background-image: url(./images/resources/slider/main-slider-2.jpg);"></div>
+							<div class="auto-container">
+								<div class="content-box">
+									<div class="content">
+										<div class="clearfix">
+											<div class="inner">
+												<h1><span>Festivals<i class="s-text">Festivals</i></span></h1>
+												<div class="travilo-text">Experience the vibrancy of local festivities,
+													joining in celebrations that showcase the heart and soul of the
+													community.</div>
+												<div class="links-box clearfix">
+													<div class="link"><a href="about.html"
+															class="theme-btn btn-style-two"><span>Explore
+																Now</span></a>
+													</div>
+													<div class="link"><a href="about.html"
+															class="theme-btn btn-style-three"><span>About
+																Us</span></a>
+													</div>
+												</div>
+											</div>
+
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<!--Slide Item-->
+						<div class="swiper-slide slide-item">
+							<div class="image-layer"
+								style="background-image: url(./images/resources/slider/main-slider-3.jpg);"></div>
+							<div class="auto-container">
+								<div class="content-box">
+									<div class="content">
+										<div class="clearfix">
+											<div class="inner">
+												<h1><span>Cuisine<i class="s-text">Cuisine</i></span></h1>
+												<div class="travilo-text">Delight in the diverse flavors and culinary
+													traditions of the region you're visiting, savoring local delicacies
+													and street food.</div>
+												<div class="links-box clearfix">
+													<div class="link"><a href="about.html"
+															class="theme-btn btn-style-two"><span>Explore
+																Now</span></a>
+													</div>
+													<div class="link"><a href="about.html"
+															class="theme-btn btn-style-three"><span>About
+																Us</span></a>
+													</div>
+												</div>
+											</div>
+
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<!--Slide Item-->
+						<div class="swiper-slide slide-item">
+							<div class="image-layer"
+								style="background-image: url(./images/resources/slider/main-slider-4.jpg);"></div>
+							<div class="auto-container">
+								<div class="content-box">
+									<div class="content">
+										<div class="clearfix">
+											<div class="inner">
+												<h1><span>Nature<i class="s-text">Nature</i></span></h1>
+												<div class="travilo-text">Immerse yourself in the natural beauty of the
+													landscapes, whether it's lush forests or pristine beaches.</div>
+												<div class="links-box clearfix">
+													<div class="link"><a href="about.html"
+															class="theme-btn btn-style-two"><span>Explore
+																Now</span></a>
+													</div>
+													<div class="link"><a href="about.html"
+															class="theme-btn btn-style-three"><span>About
+																Us</span></a>
+													</div>
+												</div>
+											</div>
+
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="swiper-button-prev"><span class="fa-solid fa-angle-left"></span></div>
+					<div class="swiper-button-next"><span class="fa-solid fa-angle-right"></span></div>
+				</div>
+			</div>
+		</div>
+		<!--End Banner Section -->
+
+		
+		
+
+		<!--Intro Section-->
+		<div class="intro-section">
+			<div class="auto-container">
+				<div class="title-box centered wow fadeInUp" data-wow-delay="0ms" data-wow-duration="1500ms">
+					<div class="subtitle">Let’s Travel Now</div>
+					<h2><span>Explore the World</span></h2>
+				</div>
+				<div class="row clearfix justify-content-center">
+					<!--Block-->
+					<div class="intro-block col-lg-4 col-md-6 col-sm-12 wow fadeInUp" data-wow-delay="0ms"
+						data-wow-duration="1500ms">
+						<div class="inner-box">
+							<div class="icon"><span><img src="./images/icons/intro-1.png" alt=""></span></div>
+							<h4>Most Popular Destinations</h4>
+							<p class="travilo-text">Discover the world's most sought-after destinations, from
+								breathtaking wonders to
+								vibrant cityscapes. Unveil iconic landmarks, immerse in diverse cultures, and create
+								cherished memories
+								on your journey of a lifetime.</p>
+						</div>
+					</div>
+					<!--Block-->
+					<div class="intro-block col-lg-4 col-md-6 col-sm-12 wow fadeInUp" data-wow-delay="300ms"
+						data-wow-duration="1500ms">
+						<div class="inner-box">
+							<div class="icon"><span><img src="./images/icons/intro-2.png" alt=""></span></div>
+							<h4>Budget Friendly Packages</h4>
+							<p class="travilo-text">Travel doesn't have to break the bank! Explore our curated selection
+								of
+								budget-friendly packages, where you can experience incredible adventures without
+								compromising on
+								quality. Embrace wanderlust without the financial worry.</p>
+						</div>
+					</div>
+					<!--Block-->
+					<div class="intro-block col-lg-4 col-md-6 col-sm-12 wow fadeInUp" data-wow-delay="600ms"
+						data-wow-duration="1500ms">
+						<div class="inner-box">
+							<div class="icon"><span><img src="./images/icons/intro-3.png" alt=""></span></div>
+							<h4>Satisfaction Guarranted</h4>
+							<p class="travilo-text">Your satisfaction is our top priority. With meticulous planning and
+								personalized
+								service, we ensure your travel experiences exceed expectations. Book with confidence,
+								knowing that your
+								journey will be nothing short of exceptional.</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<!--flights Section-->
+		<div class="packages-two">
+			<div class="bg-layer" style="background-image: url(./images/background/pattern-3.jpg);"></div>
+			<div class="auto-container">
+				<div class="tabs-box pkg-tabs">
+					<div class="title-outer clearfix">
+						<div class="title-box">
+							<div class="subtitle">Packages</div>
+							<h2><span>Top Destinations</span></h2>
+						</div>
+					</div>
+					<div class="tabs-content">
+						<div class="tab active-tab" id="pkg-tab-1">
+							<div class="row clearfix justify-content-center">
+								<!--Block-->
+                                @foreach($flights as $flight)
+								<div class="package-block alt col-lg-4 col-md-6 col-sm-12">
+                                
+                                <div class="inner-box">
+										<div class="image-box">
+											<div class="image"><a href="hotel-single.html"><img
+														src="{{ asset($flight->image) }}"
+														alt="Maldives Hotel"></a></div>
+											<div class="b-title featured"><span>  {{ $flight->name }}</span></div>
+                                            <div class="b-description"><span style="margin-top:20px;">{{ $flight->short_description }}</span></div>
+
+											<div class="fav-btn"><a href="#"><span class="far fa-heart"></span></a>
+											</div>
+										</div>
+										<div class="lower-box">
+											<div class="location">{{ $flight->name }}</div>
+											<div class="bottom-box clearfix">
+												<div class="rating"><a href="#" class="theme-btn"><i
+															class="fa-solid fa-star"></i>
+														<strong>{{$flight->rate}}</strong> &ensp;
+														<span class="guests">guests:{{$flight->guests}}</span></a></div>
+												<div class="price">Start from &ensp;<span class="amount">${{$flight->price}}</span>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+                                @endforeach
+
+							</div>
+						</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+        
+		<!--Main Footer-->
+		<footer class="main-footer style-two">
+			<div class="upper-section">
+				<div class="footer-subscribe">
+					<div class="title-box centered">
+						<div class="subtitle">Get Regular Updates</div>
+						<h2>Subscribe to our Newsletter</h2>
+					</div>
+					<div class="form-box subscribe-form">
+						<form method="post" action="https://jufailitech.com/envatoitems/travilo/html/contact.html">
+							<div class="form-group">
+								<div class="field-inner">
+									<input type="email" name="email" value="" placeholder="Type your email here"
+										required>
+								</div>
+								<button type="submit" class="theme-btn"><span>Subscribe</span></button>
+							</div>
+						</form>
+					</div>
+				</div>
+				<div class="auto-container">
+					<div class="content-box">
+						<div class="row clearfix">
+							<div class="footer-column col-xl-4 col-lg-3 col-md-6 col-sm-12">
+								<div class="footer-widget about-widget">
+									<div class="footer-logo"><a href="index-2.html" title="Travely"><img
+												src="assets/images/logo-white.png" alt="" title="Travilo"></a></div>
+									<div class="footer-info">
+										<ul class="info">
+											<li class="address"><a href="#"><i class="icon fa fa-map-marker-alt"></i>
+													20, Love Street, Muscat, Oman</a></li>
+											<li class="phone"><a href="tel:+96899999000"><i
+														class="icon fa-solid fa-phone"></i>
+													+96899999000</a></li>
+											<li class="email"><a href="mailto:hello@travilo.com"><i
+														class="icon fa fa-envelope"></i>
+													hello@travely.com</a></li>
+										</ul>
+										<ul class="social-links clearfix">
+											<li><a href="#" class="facebook"><i class="fab fa-facebook-f"></i></a></li>
+											<li><a href="#" class="twitter"><i class="fab fa-twitter"></i></a></li>
+											<li><a href="#" class="linkedin"><i class="fab fa-linkedin-in"></i></a></li>
+											<li><a href="#" class="youtube"><i class="fab fa-youtube"></i></a></li>
+										</ul>
+									</div>
+								</div>
+							</div>
+							<div class="col-xl-5 col-lg-6 col-md-6 col-sm-12">
+								<div class="row clearfix">
+
+									<div class="footer-column col-lg-6 col-md-6 col-sm-12">
+										<div class="footer-widget links-widget">
+											<h4>Top Destination</h4>
+											<div class="links">
+												<ul>
+													<li><a href="#">New York</a></li>
+													<li><a href="#">London</a></li>
+													<li><a href="#">Rome</a></li>
+													<li><a href="#">Mascot</a></li>
+													<li><a href="#">Dhaka</a></li>
+												</ul>
+											</div>
+										</div>
+									</div>
+
+									<div class="footer-column col-lg-6 col-md-6 col-sm-12">
+										<div class="footer-widget links-widget">
+											<h4>Useful Links</h4>
+											<div class="links">
+												<ul>
+													<li><a href="#">About Us</a></li>
+													<li><a href="#">Company Profile</a></li>
+													<li><a href="#">Team Members</a></li>
+													<li><a href="#">Support</a></li>
+													<li><a href="#">Career</a></li>
+												</ul>
+											</div>
+										</div>
+									</div>
+
+								</div>
+							</div>
+
+							<div class="footer-column col-xl-3 col-lg-3 col-md-6 col-sm-12">
+								<div class="footer-widget app-widget">
+									<h4>Download App</h4>
+									<!--Logo-->
+									<div class="app-info">
+										<p class="travilo-text">Download our user-friendly app for seamless access to
+											real-time updates, and
+											exclusive offers on the go.</p>
+										<div class="links">
+											<div class="link"><a href="#"><img src="assets/images/apple-store.png"
+														alt=""></a></div>
+											<div class="link"><a href="#"><img src="assets/images/play-store.png"
+														alt=""></a></div>
+										</div>
+									</div>
+								</div>
+							</div>
+
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="f-bottom">
+				<div class="auto-container">
+					<div class="inner clearfix">
+						<div class="copyright">All rights researved <strong>Travely</strong> &copy; 2023</div>
+						<div class="bottom-links">
+							<ul class="clearfix">
+								<li><a href="#">Terms & Conditions</a></li>
+								<li><a href="#">Privacy Policy</a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+
+		</footer>
+
+	</div>
+	<!--End pagewrapper-->
+
+	<!--Scroll to top-->
+	<div class="scroll-to-top scroll-to-target" data-target="html">
+		<span class="icon"><img src="assets/images/icons/arrow-up.svg" alt="" title="Go To Top"></span>
+	</div>
+
+	<script src="./js/lib/jquery.min.js"></script>
+	<script src="./js/lib/popper.min.js"></script>
+	<script src="./js/lib/bootstrap/bootstrap.min.js"></script>
+	<script src="./js/lib/jquery-ui.min.js"></script>
+	<script src="./js/lib/jquery.fancybox.min.js"></script>
+	<script src="./js/lib/isotope.min.js"></script>
+	<script src="./js/lib/slick.min.js"></script>
+	<script src="./js/lib/appear.min.js"></script>
+	<script src="./js/lib/wow.min.js"></script>
+	<script src="./js/lib/swiper.min.js"></script>
+	<script src="./js/custom-script.js"></script>
+</body>
+
+
+<!-- Mirrored from jufailitech.com/envatoitems/travilo/html/index-2.html by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 14 Nov 2023 12:53:05 GMT -->
+</html>
 @endsection
